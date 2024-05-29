d21bd7b402095bf1d59e80cb675d7494701a4de6
Penkov-D <91915267+Penkov-D@users.noreply.github.com>
2024-05-29 23:17:44 +0300

    Added loading screen

--- MSDKRemote/app/src/main/java/com/msdkremote/MainActivity.java
+++ MSDKRemote/app/src/main/java/com/msdkremote/MainActivity.java
@@ -1,15 +1,36 @@
 package com.msdkremote;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import dji.v5.manager.SDKManager;
+
 public class MainActivity extends AppCompatActivity
 {
+    private final String TAG = this.getClass().getSimpleName();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (!SDKManager.getInstance().isRegistered())
+        {
+            Log.i(TAG, "App is not registered yet!");
+            // Start the loading screen
+            this.startActivity(
+                    new Intent(this, LoadingActivity.class)
+                            .addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            );
+        }
+        else
+        {
+            Log.i(TAG, "App is registered!");
+        }
     }
 }
--- MSDKRemote/app/src/main/java/com/msdkremote/MyApplication.java
+++ MSDKRemote/app/src/main/java/com/msdkremote/MyApplication.java
@@ -20,47 +20,4 @@ public class MyApplication extends Application
         // Basically, chines viruses
         com.secneo.sdk.Helper.install(this);
     }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        // Initialize MSDK. It is recommended to put the initialization logic in Application.
-        // Of course, you can put it anywhere according to your needs.
-        SDKManager.getInstance().init(
-            this,
-            new SDKManagerCallback() {
-                @Override
-                public void onInitProcess(DJISDKInitEvent event, int totalProcess) {
-                    Log.i(TAG, "onRegisterSuccess:");
-                    if (event == DJISDKInitEvent.INITIALIZE_COMPLETE) {
-                        SDKManager.getInstance().registerApp();
-                    }
-                }
-                @Override
-                public void onRegisterSuccess() {
-                    Log.i(TAG, "onRegisterSuccess: ");
-                }
-                @Override
-                public void onRegisterFailure(IDJIError error) {
-                    Log.i(TAG, "onRegisterFailure: ");
-                }
-                @Override
-                public void onProductConnect(int productId) {
-                    Log.i(TAG, "onProductConnect: ");
-                }
-                @Override
-                public void onProductDisconnect(int productId) {
-                    Log.i(TAG, "onProductDisconnect: ");
-                }
-                @Override
-                public void onProductChanged(int productId) {
-                    Log.i(TAG, "onProductChanged: ");
-                }
-                @Override
-                public void onDatabaseDownloadProgress(long current, long total) {
-                    Log.i(TAG, String.format("onDatabaseDownloadProgress: %d / %d", current, total));
-                }
-            }
-        );
-    }
 }
